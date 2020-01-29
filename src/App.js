ab919d11c8891d77f7b3a3baf3712847a0be50b7
macgus <gustavo.adolfo.alr@gmail.com>
2020-01-29 13:16:42 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PollutionApp from './component/PollutionApp'
 
 function App() {
   return (
@@ -10,14 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <PollutionApp/>
+        </div>
       </header>
     </div>
   );
---
+++ src/component/PollutionApp.js
@@ -0,0 +1,12 @@
+import React ,{Component} from 'react'
+
+class PollutionApp extends Component{
+
+    render(){
+        return (
+            <h1> Pollution app </h1>
+        )
+    }
+}
+
+export default PollutionApp
